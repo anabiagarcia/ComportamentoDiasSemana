210dbaee08eccc7b97e531bc9a37e07339b9deaa
anabiagarcia <anabia.garcia29@gmail.com>
2026-06-26 19:33:16 -0300

    add: Coportamento para os dias da semana

--- diasSemana.py
+++ diasSemana.py
@@ -18,3 +18,80 @@ class DiaStrategy(ABC):
     def get_priority(self) -> Prioridade:
         """Retorna a prioridade do dia: ALTA, MÉDIA ou BAIXA"""
         pass
+
+class SegundaStrategy(DiaStrategy):
+    """Comportamento para Segunda-feira"""
+
+    @abstractmethod
+    def execute(self, user: str, info: str) -> str:
+        return f"{user}, Segunda-feira é dia de organizar sua rotina e a principal entrega da semana é \"{info}\"."
+
+    @abstractmethod
+    def get_priority(self) -> Prioridade:
+        return Prioridade.MEDIA
+    
+class TercaStrategy(DiaStrategy):
+    """Comportamento para Terça-feira"""
+
+    @abstractmethod
+    def execute(self, user: str, info: str) -> str:
+        return f"{user}, Terça-feira é dia de dar um gás nas pendências. Principalmente na \"{info}\" que tem o prazo mais curto."
+
+    @abstractmethod
+    def get_priority(self) -> Prioridade:
+        return Prioridade.ALTA
+    
+class QuartaStrategy(DiaStrategy):
+    """Comportamento para Quarta-feira"""
+
+    @abstractmethod
+    def execute(self, user: str, info: str) -> str:
+        return f"{user}, Quarta-Feira é dia de revisar o andamento das atividades. Lembre-se de preparar o material sobre \"{info}\" para Weekly."
+
+    @abstractmethod
+    def get_priority(self) -> Prioridade:
+        return Prioridade.MEDIA
+    
+class QuintaStrategy(DiaStrategy):
+    """Comportamento para Quinta-feira"""
+
+    @abstractmethod
+    def execute(self, user: str, info: str) -> str:
+        return f"{user}, Quinta-Feira é dia de auxiliar alguém da equipe no projeto \"{info}\"."
+
+    @abstractmethod
+    def get_priority(self) -> Prioridade:
+        return Prioridade.BAIXA
+    
+class SextaStrategy(DiaStrategy):
+    """Comportamento para Sexta-feira"""
+
+    @abstractmethod
+    def execute(self, user: str, info: str) -> str:
+        return f"{user}, Sexta-Feira é dia repassar o que foi concluído e metas cumpridas com o(a) \"{info}\"."
+
+    @abstractmethod
+    def get_priority(self) -> Prioridade:
+        return Prioridade.BAIXA
+    
+class SabadoStrategy(DiaStrategy):
+    """Comportamento para Sábado"""
+
+    @abstractmethod
+    def execute(self, user: str, info: str) -> str:
+        return f"{user}, Sábado é dia livre, aproveite para descansar e ler o livro que estava querendo: \"{info}\"."
+
+    @abstractmethod
+    def get_priority(self) -> Prioridade:
+        return Prioridade.BAIXA
+    
+class DomingoStrategy(DiaStrategy):
+    """Comportamento para Domingo"""
+
+    @abstractmethod
+    def execute(self, user: str, info: str) -> str:
+        return f"{user}, Domingo é dia de se preparar para a semana. Não esqueça que essa semana terá o evento da \"{info}\"."
+
+    @abstractmethod
+    def get_priority(self) -> Prioridade:
+        return Prioridade.BAIXA
